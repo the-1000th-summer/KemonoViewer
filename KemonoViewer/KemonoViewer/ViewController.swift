a5cc25d25a3757ccae56e94dcaa46dc16626dd8c
the-1000th-summer <denghao775785604@163.com>
2025-06-11 21:03:04 +0800

    first commit

---
+++ KemonoViewer/KemonoViewer/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  KemonoViewer
+//
+//  Created on 2025/6/11.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
